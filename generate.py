44c3fd2a8bfcccb3bbe9f82c83edeee072b763b7
ihseh <hmuntiu@gmail.com>
2023-01-30 21:44:57 -0500

    3 links 2 joints

--- generate.py
+++ generate.py
@@ -16,6 +16,9 @@ def Create_Robot():
 	pyrosim.Send_Cube(name="Link0", pos=[x,y,z] , size=[length,width,height])
 	pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [x+.5,y,z+.5])
 	pyrosim.Send_Cube(name="Link1", pos=[.5,0,.5] , size=[length,width,height])
+	pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [1,y,1])
+	pyrosim.Send_Cube(name="Link2", pos=[.5,0,.5] , size=[length,width,height])
+
 	pyrosim.End()
 
 
