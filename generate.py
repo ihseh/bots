1f9efa1613b691421faedf736d2ed3c47c542bef
ihseh <hmuntiu@gmail.com>
2023-01-30 21:41:57 -0500

    joints2

--- generate.py
+++ generate.py
@@ -15,7 +15,7 @@ def Create_Robot():
 	pyrosim.Start_URDF("body.urdf")
 	pyrosim.Send_Cube(name="Link0", pos=[x,y,z] , size=[length,width,height])
 	pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [x+.5,y,z+.5])
-	pyrosim.Send_Cube(name="Link1", pos=[x+.5,y,z] , size=[length,width,height])
+	pyrosim.Send_Cube(name="Link1", pos=[.5,0,.5] , size=[length,width,height])
 	pyrosim.End()
 
 
