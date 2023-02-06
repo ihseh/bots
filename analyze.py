1af828a0119c9a955d478f5d3457c738fbecb56b
ihseh <hmuntiu@gmail.com>
2023-02-06 09:33:34 -0500

    analyze done

--- analyze.py
+++ analyze.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as matplotlib
-backLegSensorValues = np.load("data/sensorVals.npy")
-print(backLegSensorValues)
-matplotlib.plot(backLegSensorValues)
+backLegSensorValues = np.load("data/backLegSensorVals.npy")
+frontLegSensorValues = np.load("data/frontLegSensorVals.npy")
+#print(backLegSensorValues)
+matplotlib.plot(backLegSensorValues, label = "Back Leg", linewidth = 4)
+matplotlib.plot(frontLegSensorValues, label = "Front Leg")
+matplotlib.legend()
 matplotlib.show()
--- simulate.py
+++ simulate.py
@@ -11,12 +11,16 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = np.zeros(100)
+frontLegSensorValues = np.zeros(100)
 for x in range(100):
 	p.stepSimulation()
 	backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
+	frontLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("Frontleg")
 	time.sleep(1/60)
 	#print(x)
 p.disconnect()
-print(backLegSensorValues)
-np.save("data/sensorVals.npy", backLegSensorValues)
+#print(backLegSensorValues)
+np.save("data/backLegSensorVals.npy", backLegSensorValues)
+np.save("data/frontLegSensorVals.npy", frontLegSensorValues)
+
 
