ac81024f96f432fed010c10b1a45542b8246cf09
ihseh <hmuntiu@gmail.com>
2023-02-13 14:39:06 -0500

    legs

--- simulate.py
+++ simulate.py
@@ -13,15 +13,27 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 length = 1000
-amplitude = math.pi/4
-frequency = 10
-phaseOffset = 0
 backLegSensorValues = np.zeros(length)
 frontLegSensorValues = np.zeros(length)
-a = np.linspace(0, 2*math.pi, length)
-targetAngles = np.array(amplitude * np.sin(frequency * a + phaseOffset))
-np.save("data/targetAngles.npy", targetAngles)
-exit("OK LEAVING NOW. YEAH, I THINK IM GONNA LEAVE NOW.")
+
+#Motor Values
+
+#Backleg
+amplitude_Backleg = math.pi/4
+frequency_Backleg = 10
+phaseOffset_Backleg = 0
+a_Backleg = np.linspace(0, 2*math.pi, length)
+targetAngles_Backleg = np.array(amplitude_Backleg * np.sin(frequency_Backleg * a_Backleg + phaseOffset_Backleg))
+
+#FrontLeg
+amplitude_Frontleg = math.pi/4
+frequency_Frontleg = 10
+phaseOffset_Frontleg = 0
+a_Frontleg = np.linspace(0, 2*math.pi, length)
+targetAngles_Frontleg = np.array(amplitude_Frontleg * np.sin(frequency_Frontleg * a_Frontleg + phaseOffset_Frontleg))
+
+#np.save("data/targetAngles.npy", targetAngles)
+#exit("OK LEAVING NOW. YEAH, I THINK IM GONNA LEAVE NOW.")
 for x in range(length):
 	p.stepSimulation()
 	backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
@@ -30,13 +42,13 @@ for x in range(length):
 	bodyIndex = robotId,
 	jointName = b'Torso_Backleg',
 	controlMode = p.POSITION_CONTROL,
-	targetPosition = targetAngles[x],
+	targetPosition = targetAngles_Backleg[x],
 	maxForce = 100)
 	pyrosim.Set_Motor_For_Joint(
 	bodyIndex = robotId,
 	jointName = b'Torso_Frontleg',
 	controlMode = p.POSITION_CONTROL,
-	targetPosition = targetAngles[x],
+	targetPosition = targetAngles_Frontleg[x],
 	maxForce = 100)
 	time.sleep(1/400)
 	#print(x)
