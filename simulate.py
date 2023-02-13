8e99d9e965bc2a33c589748b4995bcf1dea0c4bf
ihseh <hmuntiu@gmail.com>
2023-02-13 11:23:40 -0500

    no movement

--- simulate.py
+++ simulate.py
@@ -10,12 +10,18 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
-backLegSensorValues = np.zeros(100)
-frontLegSensorValues = np.zeros(100)
-for x in range(100):
+backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
+for x in range(1000):
 	p.stepSimulation()
 	backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
 	frontLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("Frontleg")
+	pyrosim.Set_Motor_For_Joint(
+	bodyIndex = robotId,
+	jointName = b'Torso_Backleg',
+	controlMode = p.POSITION_CONTROL,
+	targetPosition = 0.0,
+	maxForce = 500)
 	time.sleep(1/60)
 	#print(x)
 p.disconnect()
