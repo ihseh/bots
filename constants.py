f17ad40301a92560858ead415300551fcff8b44f
ihseh <hmuntiu@gmail.com>
2023-02-20 20:29:12 -0500

    constants.py created

---
+++ constants.py
@@ -0,0 +1,10 @@
+import math
+
+
+amplitude_Backleg = math.pi/4
+frequency_Backleg = 10
+phaseOffset_Backleg = 0
+
+amplitude_Frontleg = math.pi/4
+frequency_Frontleg = 10
+phaseOffset_Frontleg = math.pi/8
