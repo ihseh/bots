aeb88cdf158aea19a079c138cb04ccd6d6752461
ihseh <hmuntiu@gmail.com>
2023-02-20 20:28:47 -0500

    constants.py created

--- simulate.py
+++ simulate.py
@@ -5,6 +5,8 @@ import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import math
 import random
+import constants as c
+
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
@@ -19,18 +21,18 @@ frontLegSensorValues = np.zeros(length)
 #Motor Values
 
 #Backleg
-amplitude_Backleg = math.pi/4
-frequency_Backleg = 10
-phaseOffset_Backleg = 0
+# amplitude_Backleg = math.pi/4
+# frequency_Backleg = 10
+# phaseOffset_Backleg = 0
 a_Backleg = np.linspace(0, 2*math.pi, length)
-targetAngles_Backleg = np.array(amplitude_Backleg * np.sin(frequency_Backleg * a_Backleg + phaseOffset_Backleg))
+targetAngles_Backleg = np.array(c.amplitude_Backleg * np.sin(c.frequency_Backleg * a_Backleg + c.phaseOffset_Backleg))
 
 #FrontLeg
-amplitude_Frontleg = math.pi/4
-frequency_Frontleg = 10
-phaseOffset_Frontleg = math.pi/8
+# amplitude_Frontleg = math.pi/4
+# frequency_Frontleg = 10
+# phaseOffset_Frontleg = math.pi/8
 a_Frontleg = np.linspace(0, 2*math.pi, length)
-targetAngles_Frontleg = np.array(amplitude_Frontleg * np.sin(frequency_Frontleg * a_Frontleg + phaseOffset_Frontleg))
+targetAngles_Frontleg = np.array(c.amplitude_Frontleg * np.sin(c.frequency_Frontleg * a_Frontleg + c.phaseOffset_Frontleg))
 
 #np.save("data/targetAnglesBackleg.npy", targetAngles_Backleg)
 #np.save("data/targetAnglesFrontleg.npy", targetAngles_Frontleg)
