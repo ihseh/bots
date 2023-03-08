bcb8b0680f544045f1fa8bbf0b58f048d8a39337
ihseh <hmuntiu@gmail.com>
2023-03-07 20:44:41 -0500

    fixed keyError problem

--- generate.py
+++ generate.py
@@ -45,7 +45,7 @@ def Generate_Brain():
 	pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
 	pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "Backleg")
 	pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "Frontleg")
-	pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
+	pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_Backleg")
 	pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_Frontleg")
 
 
